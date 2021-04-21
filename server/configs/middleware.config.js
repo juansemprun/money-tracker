5a3714f8f183858c4289ee105865402f8812a619
Juan Semprun <juand.semprunr@gmail.com>
2021-04-22 00:01:20 +0200

    Update cors options

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -14,7 +14,7 @@ const corsOptions = {
 }
 
 module.exports = app => {
-    app.use(cors(corsOptions))
+    app.use(cors())
     app.use(logger('dev'))
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
